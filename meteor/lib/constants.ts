8eaa994ee014102df714aae6227c73f4f9ffe81d
Jan Starzak <jan.starzak@gmail.com>
2022-05-11 10:46:19 +0200

    chore: lint

--- meteor/lib/constants.ts
+++ meteor/lib/constants.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = 'Jǫrmungandr'
+export const APP_NAME = 'Sofie E2E Tests'
 export const APP_COPYRIGHT = '2022 NRK'
 export const APP_GITHUB_LINK = 'https://github.com/nrkno/sofie-e2e-test-runner'
